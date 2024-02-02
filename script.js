c9c16132de39aa863267e58cf1a4948b56e9affa
Tech Nik <tech.nik.effects@gmail.com>
2024-02-02 17:14:30 +0100

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function downloadFile(url) {
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = url.split("/").pop();
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+
+
+function download_database() {
+    downloadFile("Database - 02.02.24.kdbx")
+}
